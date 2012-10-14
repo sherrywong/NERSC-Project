90aaa9027761bc5110666c9534b86f333844a0ec
sherrywong <sherrywong@berkeley.edu>
2012-10-14 13:11:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NerscProject::Application.config.secret_token = 'f88a66b1c1bebe0b8a222140c8c9031931e37329f0d44e321ba3b9df8f3d03986ffe3b989381508973655f183d98a4b65bb6978375c60486d3ac39bc58071911'
