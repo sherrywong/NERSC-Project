2fda2f2449c19dda99ff7d0da8ce29e992d696e1
sherrywong <sherrywong@berkeley.edu>
2012-10-20 19:51:37 -0700

    added create to user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,17 +1,22 @@
-class HomeController < ApplicationController  
+class UserController < ApplicationController
   #You can find "login_required" in application_controller.rb"
   before_filter :login_required, :except => [:login]
 
-  def login 
+  def create
+    user_hash = [:email => params[:email], :first => params[:first], :last => params[:last], :password => params[:first], :username => params[:usrname]]
+    User.add_new_user(user_hash)
+  end
+
+  def login
     if request.post? #If the form was submitted
       user = Users.find(:first, :conditions=>['username=?',(params[:username])]) #Find the user based on the name submitted
       if !user.nil? && user.password==params[:password] #Check that this user exists and it's password matches the inputted password
         session[:uid] = user.id #If so log in the user
         redirect_to :action => "show_profile" #And redirect to their profile
-      elsif user.nil? 
+      elsif user.nil?
         redirect_to :action => "login", :notice=> "We don't have a user by this username. Please contact an administrator to be granted access to the application."
-      else user.password!=params[:password] 
-        redirect_to :action => "login", :notice=> "Incorrect password. Please try again."         
+      else user.password!=params[:password]
+        redirect_to :action => "login", :notice=> "Incorrect password. Please try again."
       end
     end
   end
