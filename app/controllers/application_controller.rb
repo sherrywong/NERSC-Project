12ecd1f7dd7b674075ff189fda468e078cd45e0f
sherrywong <sherrywong@berkeley.edu>
2012-10-20 19:19:23 -0700

    added application_controller and user_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def login_required
+    if session[:uid] #if there is a logged in user
+      return true
+    end #Otherwise redirect to login page
+    redirect_to :controller => "login", :action=> "login", :notice=>"Please log in to view this page"
+    return false 
+  end
 end
---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController  
+  #You can find "login_required" in application_controller.rb"
+  before_filter :login_required, :except => [:login]
+
+  def login 
+    if request.post? #If the form was submitted
+      user = Users.find(:first, :conditions=>['username=?',(params[:username])]) #Find the user based on the name submitted
+      if !user.nil? && user.password==params[:password] #Check that this user exists and it's password matches the inputted password
+        session[:uid] = user.id #If so log in the user
+        redirect_to :action => "show_profile" #And redirect to their profile
+      elsif user.nil? 
+        redirect_to :action => "login", :notice=> "We don't have a user by this username. Please contact an administrator to be granted access to the application."
+      else user.password!=params[:password] 
+        redirect_to :action => "login", :notice=> "Incorrect password. Please try again."         
+      end
+    end
+  end
+
+  def logout
+    session[:uid] = nil #Logs out the user
+    redirect_to :action => "login" #redirect to log-in screen
+  end
+
+end
