d96211245e816cfc48ae2baeb80b7dd001fb1873
sherrywong <sherrywong@berkeley.edu>
2012-10-28 12:04:06 -0700

    changed create and destroy for user controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -20,15 +20,18 @@ class UserController < ApplicationController
   def new
     if request.post?
 
-      user_hash = params[:user]
-    User.new(user_hash).save
+    user_hash = params[:user]
+    User.add_new_user(user_hash)
     redirect_to "/user/show_users", :notice => "User created."
       end
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy if User.count > 1
+    if User.count > 1
+      User.deactivate_user(params[:id])
+      @user.destroy
+    end
     flash[:notice] = "User '#{@user.first}' '#{@user.last}' deleted."
     redirect_to user_path
   end
@@ -49,7 +52,7 @@ class UserController < ApplicationController
         redirect_to :action => "index" #And redirect to their profile
       elsif User.find(:first, :conditions=>['username=?',(params[:username])])==nil
         redirect_to :action => "login", :notice=> "We don't have a user by this username. Please contact an administrator to be granted access to the application."
-      else 
+      else
         redirect_to :action => "login", :notice=> "Incorrect password. Please try again."
       end
     end
