f1c0c61caadd33b39d8e3d81b0bd86cb02d2dbad
sherrywong <sherrywong@berkeley.edu>
2012-11-04 17:26:11 -0800

    got over 85% coverage

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,12 +4,12 @@ class Project < ActiveRecord::Base
   validates_presence_of :name
   validates_uniqueness_of :name
   validates_inclusion_of :status, :in=>["active", "retired", "pending"]
-  
+
   has_many :project_memberships, :dependent => :destroy #assume destroy for now.
   has_many :users, :through => :project_memberships #identical to members
   has_many :members, :through => :project_memberships, :source => :user   #identical to users, this is preferred.
   has_many :risks
-  
+
   #Will merge current list with input list, assuming read-only access
   def add_members(members_id_list) #can also just provide the users themselves.
     members_id_list.each do |member_id|
@@ -20,7 +20,7 @@ class Project < ActiveRecord::Base
     self.save
   end
 
-=begin  
+=begin
   def add_member(member_id)
     member = User.find_by_id(member_id)
     ProjectMembership.new(:user_id => member_id, :project_id => self.id)
@@ -28,7 +28,7 @@ class Project < ActiveRecord::Base
     self.save
   end
 =end
-  
+
   def remove_member(member_id)
     @pm = ProjectMembership.find_by_project_id_and_user_id(self.id, member_id)
     if @pm
@@ -37,7 +37,7 @@ class Project < ActiveRecord::Base
     end
   end
 
-=begin  
+=begin
   def remove_members(members_id_list)
     members_id_list.each do |member_id|
         member = User.find_by_id(member_id)
@@ -47,7 +47,7 @@ class Project < ActiveRecord::Base
     self.save
   end
 =end
-  
+
   #permission must be either read or write (or more importantly, must
   #match whatever the ProjectMembership validation requires).
   def edit_member_permission(member_id, perm)
@@ -59,20 +59,20 @@ class Project < ActiveRecord::Base
     return @pm
   end
 
-  
+
   def has_member?(member_id)
     return self.members.include? User.find_by_id(member_id)
   end
-  
+
   def owner
     ProjectMembership.owner_of_project(self)
   end
 
-=begin 
+
   def owner= (new_owner)
     ProjectMembership.set_owner_of_project(self, new_owner)
   end
-=end
 
-  
+
+
 end
