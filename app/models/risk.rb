f2660d5319bd496fe46440aedf6b87fa5ec3a2e0
Linda Zhang <zhang.lynda@gmail.com>
2012-11-18 02:42:55 +0000

    changed order of validations

--- app/models/risk.rb
+++ app/models/risk.rb
@@ -3,10 +3,10 @@ class Risk < ActiveRecord::Base
   has_paper_trail :on => [:update, :destroy]
 
    attr_accessible :title, :short_title, :description, :root_cause, :mitigation, :contingency, :cost, :schedule, :technical, :other_type, :probability, :status, :early_impact, :last_impact, :type, :critical_path, :wbs_spec, :comment, :owner_id, :project_id, :creator_id
+
+   validates_inclusion_of :probability, :cost, :schedule, :technical, :in => [3, 2, 1]
+   validates_inclusion_of :other_type,  :in => [3, 2, 1], :allow_nil=> true
    validates_presence_of :title, :description, :probability, :cost, :schedule, :technical, :status, :early_impact, :last_impact, :days_to_impact, :owner_id, :project_id, :creator_id
-
-   validates_inclusion_of :probability, :cost, :schedule, :technical, :other_type,  :in => [3, 2, 1], :allow_nil=> true
-
    #validates_uniqueness_of :title, :scope => :project_id #no longer required!
     #risks are uniquely identified by proj-prefix + risk_id.
 
@@ -25,6 +25,19 @@ class Risk < ActiveRecord::Base
     end
 =end
 
+    def map_to_int(string)
+	if string == "High"
+	  return 3
+       elsif string == "Medium"
+         return 2
+       else
+         return 1
+       end
+    end
+
+    def find_username(user_id)
+	return User.find_by_id(user_id).username
+    end
 
     def owner_exists?(user_id)
         if User.find_by_id(user_id)==nil
