8c2cbddd1d5b8c88afb12728a0baf237a7fe85f8
sherrywong <sherrywong@berkeley.edu>
2012-12-05 12:07:57 -0800

    reverted reminder

--- config/initializers/reminder_scheduler.rb
+++ config/initializers/reminder_scheduler.rb
@@ -1,6 +1,6 @@
 scheduler = Rufus::Scheduler.start_new
 
-scheduler.every '1d', :first_in => '1m' do
+scheduler.every '1d' do
   for risk in Risk.all do
     Risk.notification_emailrisk_notify(risk)
   end
